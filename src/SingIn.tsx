4ab85b8766e83afe6779036cb99b606462864578
ezequielalonso9 <ezequiel.javier.alonso@gmail.com>
2023-01-04 12:27:51 -0300

    create SingIn

---
+++ src/SingIn.tsx
@@ -0,0 +1,182 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+import { Alert, Collapse, IconButton } from '@mui/material';
+import CloseIcon  from '@mui/icons-material/Close';
+
+
+const theme = createTheme();
+
+type UserAuth = {
+  username: string
+  password: string
+}
+
+type UserCheck = {
+  username: boolean
+  password: boolean
+}
+
+// type Token = string;
+
+export default function SignIn() {
+
+  const [userAuth, setUserAuth] = useState<UserAuth>({
+    username: "",
+    password: ""
+  })
+
+  const [fieldIsOk, setFieldIsOk] = useState<UserCheck>({
+    username: false,
+    password: false
+  })
+
+  const [isOk, setIsOk] = useState(false)
+
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    isAllOk()
+  }, [userAuth])
+
+
+  const isAllOk = () => {
+    let isAllOk = Object.values(fieldIsOk).every(value => value === true)
+    if (isAllOk) {
+      setIsOk(true)
+    } else {
+      setIsOk(false)
+    }
+  }
+
+
+  const validateField = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+
+    if (e.target.value.length <= 0) {
+      console.log(`key: ${e.target.name} value.length: ${e.target.value.length}`)
+      setFieldIsOk(fieldIsOk => ({
+        ...fieldIsOk,
+        [e.target.name]: false
+      }))
+    } else {
+      setFieldIsOk(fieldIsOk => ({
+        ...fieldIsOk,
+        [e.target.name]: true
+      }))
+    }
+
+  }
+
+
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios.get("http://localhost:8080/auth", {
+      auth: userAuth
+    }).then(res => console.log(res.data))
+
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setUserAuth({
+      ...userAuth,
+      [e.target.name]: e.target.value
+    })
+    validateField(e)
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              onChange={handleChange}
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="User name"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              onChange={handleChange}
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              disabled={!isOk}
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2" onClick={() => { setOpen(true) }}>
+                  Forgot password or username?
+                </Link>
+                <Collapse in={open}>
+                  <Alert
+                    severity='info'
+                    action={
+                      <IconButton
+                        aria-label="close"
+                        color="inherit"
+                        size="small"
+                        onClick={() => {
+                          setOpen(false);
+                        }}
+                      >
+                        <CloseIcon fontSize="inherit" />
+                      </IconButton>
+                    }
+                    sx={{ m: 2 }}
+                  >
+                   Please contact the administrator 
+                  </Alert>
+                </Collapse>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
